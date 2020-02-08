74291d193e488132e8f8a294b964e9e895ab8f3a
Lucas Felipe Costa da Mata <lucasfelipecdm@hotmail.com>
2020-02-07 23:53:54 -0300

    :art: Change folders structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
+const { token, prefix } = require('./config/config.json');
 
 const client = new Discord.Client();
 
 client.once('ready', ()=> console.log('Ready!'));
 
-client.login('');
+client.login(token);
